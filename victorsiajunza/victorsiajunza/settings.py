9b856cb1c97bef1cf85a842ce94d65d14ea7485d
victorsiajunza <logzee26@gmail.com>
2022-06-24 23:12:07 +1000

    registered post

--- victorsiajunza/victorsiajunza/settings.py
+++ victorsiajunza/victorsiajunza/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog'
+    
 ]
 
 ROOT_URLCONF = 'victorsiajunza.urls'
