b5dd209006f57ea966165bb176a8dede2ac2921e
victorsiajunza <logzee26@gmail.com>
2022-06-25 00:30:28 +1000

    Created templates

--- victorsiajunza/blog/views.py
+++ victorsiajunza/blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    model: Post
+
+class PostCreateView(CreateView):
+     model = Post
+
+fields = '__all__'
+
+success_url  = reverse_lazy('blog:all')
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+fields = '__all__'
+
+success_url  = reverse_lazy('blog:all')
+
+class PostDeleteView(DeleteView):
+    model = Post
+
+fields = '__all__'
+
+success_url  = reverse_lazy('blog:all')
+
